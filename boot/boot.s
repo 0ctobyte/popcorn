6c2889e8300eedf2abebd45afec36525b3dfc126
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2014-01-29 18:49:24 -0500

    Setup stacks for all privileged processor modes

--- boot/boot.s
+++ boot/boot.s
@@ -5,33 +5,55 @@
 
 # Setup the stacks in the BSS
 .comm __svc_stack_bottom, 4096, 4
+.comm __abt_stack_bottom, 4096, 4
 .comm __irq_stack_bottom, 4096, 4
+.comm __fiq_stack_bottom, 4096, 4
+.comm __und_stack_bottom, 4096, 4
+.comm __mon_stack_bottom, 4096, 4
 
 # Align to 4 byte (word) boundary, the 2 specifies the # of zeros in the low
 # order bits
 .align 2 
 _start:
-
 	# Set the svc stack
 	LDR SP, =__svc_stack_bottom+4096
-	
-	# Get program status register
+
+	# Get the program status register	
 	MRS R0, CPSR
-	
-	# Clear mode bits
-	BIC R1, R0, #0x1F
-	
-	# Go into IRQ mode
-	ORR R1, R1, #0x12
-	
-	MSR CPSR, R1
-	
+
 	# Set IRQ mode stack, SP (and LR) is banked in IRQ mode
+	BIC R1, R0, #0x1F
+	ORR R1, R1, #0x12
+	MSR CPSR, R1
 	LDR SP, =__irq_stack_bottom+4096
+
+	# Set up the FIQ mode stack
+	BIC R1, R0, #0x1F
+	ORR R1, R1, #0x11
+	MSR CPSR, R1
+	LDR SP, =__fiq_stack_bottom+4096
+	
+	# Set up the Abort mode stack
+	BIC R1, R0, #0x1F
+	ORR R1, R1, #0x17
+	MSR CPSR, R1
+	LDR SP, =__abt_stack_bottom+4096
+
+	# Set up the Undefined mode stack
+	BIC R1, R0, #0x1F
+	ORR R1, R1, #0x1B
+	MSR CPSR, R1
+	LDR SP, =__und_stack_bottom+4096
+
+	# Set up the Monitor mode stack
+	BIC R1, R0, #0x1F
+	ORR R1, R1, #0x16
+	MSR CPSR, R1
+	LDR SP, =__mon_stack_bottom+4096
 
 	# Go back to supervisor mode
 	MSR CPSR, R0
-	
+
 	# Jump to kernel main
 	BL kmain
 
