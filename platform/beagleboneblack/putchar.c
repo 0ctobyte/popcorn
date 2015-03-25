65705340cca9d809307d0948da85aca2a5460099
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-03-25 14:26:11 -0400

    Fix putchar.c: Moved carriage return code to kputc

--- platform/beagleboneblack/putchar.c
+++ platform/beagleboneblack/putchar.c
@@ -5,8 +5,6 @@
 int32_t putchar(char c) {
 	if(REG_RD32(R_UART0_SSR) & 1) return(-1);
 	REG_WR32(R_UART0_THR, c);
-  // Send a carriage return after a newline
-  if(c == '\n') while(putchar('\r') == -1) continue;
-	return(0);
+  return(0);
 }
 
