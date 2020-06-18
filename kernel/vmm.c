492c3449d3fc3eb5efd396dea8af1d2ee8bc3e08
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2020-06-17 17:07:35 -0700

    vmm: update uart_base_va

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -14,7 +14,7 @@ void vmm_init(void) {
 
     // FIXME switching UART to VA address
     extern unsigned long uart_base_addr;
-    vaddr_t uart_base_va = 0xFFFFC00000000000;
+    vaddr_t uart_base_va = 0xFFFFFFFFC0000000;
     pmap_kenter_pa(uart_base_va, uart_base_addr & ~(PAGESIZE - 1), VM_PROT_DEFAULT, PMAP_FLAGS_READ | PMAP_FLAGS_WRITE | PMAP_FLAGS_NOCACHE);
     uart_base_addr = uart_base_va + (uart_base_addr & (PAGESIZE - 1));
 
