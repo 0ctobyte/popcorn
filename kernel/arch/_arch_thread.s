71823e2880a4937ed2eafe5a3b1375b5c4bb694e
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2020-07-10 06:43:44 -0700

    arch_thread: Don't modify sp in save_context in case calling function uses to access stack frame

--- kernel/arch/_arch_thread.s
+++ kernel/arch/_arch_thread.s
@@ -32,25 +32,27 @@ arch_thread_load_context:
 .global arch_thread_save_context
 .align 2
 arch_thread_save_context:
-    // First four words on stack are FAR, ESR, ELR and SPSR. These are zero'd out
-    stp xzr, xzr, [sp, #-16]!
-    stp xzr, xzr, [sp, #-16]!
-    stp x30, xzr, [sp, #-16]!
-    stp x28, x29, [sp, #-16]!
-    stp x26, x27, [sp, #-16]!
-    stp x24, x25, [sp, #-16]!
-    stp x22, x23, [sp, #-16]!
-    stp x20, x21, [sp, #-16]!
-    stp x18, x19, [sp, #-16]!
-    stp x16, x17, [sp, #-16]!
-    stp x14, x15, [sp, #-16]!
-    stp x12, x13, [sp, #-16]!
-    stp x10, x11, [sp, #-16]!
-    stp x8, x9, [sp, #-16]!
-    stp x6, x7, [sp, #-16]!
-    stp x4, x5, [sp, #-16]!
-    stp x2, x3, [sp, #-16]!
-    stp x0, x1, [sp, #-16]!
-
     mov x0, sp
+
+    // First four words on stack are FAR, ESR, ELR and SPSR. These are zero'd out
+    stp xzr, xzr, [x0, #-16]!
+    stp xzr, xzr, [x0, #-16]!
+    stp x30, xzr, [x0, #-16]!
+    stp x28, x29, [x0, #-16]!
+    stp x26, x27, [x0, #-16]!
+    stp x24, x25, [x0, #-16]!
+    stp x22, x23, [x0, #-16]!
+    stp x20, x21, [x0, #-16]!
+    stp x18, x19, [x0, #-16]!
+    stp x16, x17, [x0, #-16]!
+    stp x14, x15, [x0, #-16]!
+    stp x12, x13, [x0, #-16]!
+    stp x10, x11, [x0, #-16]!
+    stp x8, x9, [x0, #-16]!
+    stp x6, x7, [x0, #-16]!
+    stp x4, x5, [x0, #-16]!
+    stp x2, x3, [x0, #-16]!
+    stp x0, x1, [x0, #-16]
+    sub x0, x0, #16
+
     ret lr
