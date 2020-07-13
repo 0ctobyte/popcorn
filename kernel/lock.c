07c7144427203a7f80619578c262e555f185b05a
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2020-07-13 12:44:31 -0700

    lock: Implement try_acquire functions

--- kernel/lock.c
+++ kernel/lock.c
@@ -47,6 +47,39 @@ void lock_acquire_shared(lock_t *lock) {
     spinlock_irq_release(&lock->interlock);
 }
 
+bool lock_try_acquire_exclusive(lock_t *lock) {
+    kassert(lock != NULL);
+
+    spinlock_irq_acquire(&lock->interlock);
+
+    if (lock->state != LOCK_STATE_FREE) return false;
+
+    lock->thread = proc_thread_current();
+    lock->state = LOCK_STATE_EXCLUSIVE;
+
+    spinlock_irq_release(&lock->interlock);
+
+    return true;
+}
+
+bool lock_try_acquire_shared(lock_t *lock) {
+    kassert(lock != NULL);
+
+    spinlock_irq_acquire(&lock->interlock);
+
+    if (lock->state == LOCK_STATE_EXCLUSIVE
+        || (lock->state == LOCK_STATE_EXCLUSIVE_UPGRADE && proc_thread_current()->priority > lock->thread->priority)) {
+        return false;
+    }
+
+    lock->shared_count++;
+    if (lock->state != LOCK_STATE_EXCLUSIVE_UPGRADE) lock->state = LOCK_STATE_SHARED;
+
+    spinlock_irq_release(&lock->interlock);
+
+    return true;
+}
+
 void lock_release_exclusive(lock_t *lock) {
     kassert(lock != NULL);
 
